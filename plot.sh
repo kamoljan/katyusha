203db67c780a0ecf3290afea514493ddec551326
Kamol Mavlonov <kamol@701search.com>
2014-05-24 23:46:51 +0800

    Plot.sh update

--- plot.sh
+++ plot.sh
@@ -5,11 +5,11 @@ echo 'Concurrency ' $CONCURRENCY
 
 # cmd="./katyusha $CONCURRENCY > stats.csv"
 # echo $cmd
+# set title  "'$CONCURRENCY' concurrent connections"; \
 echo '\
 set terminal png font "/Library/Fonts/Times\ New\ Roman.ttf" 14; \
 set output "'$CONCURRENCY'"; \
 set datafile separator ","; \
-set title  "'$CONCURRENCY' concurrent connections"; \
-set xlabel "Percentages served"; \
+set xlabel "Concurrent connections"; \
 set ylabel "Response time (milliseconds)"; \
-plot "stats.csv" notitle' | gnuplot -persist
+plot "stats'$CONCURRENCY'.csv" notitle' | gnuplot -persist
