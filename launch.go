78846811292822dd0709c8fe84d40df14fc62515
Kamol Mavlonov <kamol@701search.com>
2014-05-25 15:33:25 +0800

    Url is now dynamic

--- launch.go
+++ launch.go
@@ -42,10 +42,10 @@ func newfileUploadRequest(uri string, file string) (*http.Request, error) {
 	return req, err
 }
 
-func lauchOneRocket(i int, file string) {
+func lauchOneRocket(url string, i int, file string) {
 	t0 := time.Now()
 
-	request, err := newfileUploadRequest("http://127.0.0.1:9090", file)
+	request, err := newfileUploadRequest(url, file)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -74,7 +74,7 @@ func lauchOneRocket(i int, file string) {
 	}
 }
 
-func lauchKatyusha(rockets int) {
+func lauchKatyusha(url string, rockets int) {
 	file, err := os.Open("rockets.txt")
 	if err != nil {
 		panic(err)
@@ -86,19 +86,20 @@ func lauchKatyusha(rockets int) {
 	for scanner.Scan() {
 		i = i + 1
 		if i <= rockets {
-			go lauchOneRocket(i, scanner.Text())
+			go lauchOneRocket(url, i, scanner.Text())
 		}
 	}
 }
 
 func main() {
 	flag.Parse()
-	rockets, err := strconv.Atoi(flag.Arg(0))
+	url := flag.Arg(0)
+	rockets, err := strconv.Atoi(flag.Arg(1))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
-	lauchKatyusha(rockets)
+	lauchKatyusha(url, rockets)
 
 	var input string
 	fmt.Scanln(&input)
